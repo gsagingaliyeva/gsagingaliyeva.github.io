36dcf3826a889c9a9e25b83867ac8be81c987c48
gsagingaliyeva <82785199+gsagingaliyeva@users.noreply.github.com>
2021-07-06 20:44:25 -0400

    Add files via upload

--- Prime.py
+++ Prime.py
@@ -1,11 +1,12 @@
 def is_prime(a):
-    n=a
-    Prime=[2,3,5,7,11]
-    if n<=1: 
-        print ("False")
-    elif n in Prime:
-        print ("True")
-    elif n not in Prime and n%2==0 or n%3==0 or n%4==0 or n%5==0 or n%6==0 or n%7==0 or n%8==0 or n%9==0:
-        print ("False")
-    else:
-        print ("True")
+    i=2
+    Prime = "True" 
+    while i<a:
+        if a%i==0: 
+            Prime = "False"
+        i=i+1
+    
+    if a==1:
+        Prime = "False"
+
+    print (Prime)
